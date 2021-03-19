a153f1d0d098ef33efbff0161073eb524f9c4b64
saitharunthodupunuru <saitharun.thodupunuru@scality.com>
2021-03-19 00:36:38 -0700

    feature: S3C-3525 Enabling Swagger documentation

---
+++ osis-core/src/main/java/com/scality/osis/configuration/DocumentationConfig.java
@@ -0,0 +1,62 @@
+/**
+ *Copyright 2020 VMware, Inc.
+ *Copyright 2021 Scality, Inc.
+ *SPDX-License-Identifier: Apache License 2.0
+ */
+
+package com.scality.osis.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+@Primary
+public class DocumentationConfig {
+    private static final String DOC_TITLE = "Object Storage Interoperability Services API for Scality platform";
+    private static final String DOC_DESCRIPTION = "This is VMware Cloud Director Object Storage Interoperability Services API for Scality platform.";
+    private static final String DOC_VERSION = "1.0.0";
+    private static final String PROJECT_BASE = "com.vmware.osis";
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title(DOC_TITLE)
+            .description(DOC_DESCRIPTION)
+            .version(DOC_VERSION)
+            .build();
+    }
+
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
+    }
+
+    @Bean
+    public Docket customImplementation(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(true)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(PROJECT_BASE))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
--- src/main/java/com/scality/osis/Application.java
+++ src/main/java/com/scality/osis/Application.java
@@ -9,12 +9,14 @@ package com.scality.osis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author ges
  */
 @ComponentScan(basePackages = {"com.vmware.osis","com.scality.osis"})
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
     public static void main(String[] args) throws Exception {
         new SpringApplication(Application.class).run(args);
