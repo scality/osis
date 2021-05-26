44459f545eeb39b0d462a336278cd968507adb88
saitharunthodupunuru <saitharun.thodupunuru@scality.com>
2021-05-26 02:31:46 -0700

    feature:S3C-4408 Implement an external config file for cryptography

---
+++ osis-core/src/main/java/com/scality/osis/utils/YamlPropertySourceFactory.java
@@ -0,0 +1,32 @@
+package com.scality.osis.utils;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class YamlPropertySourceFactory implements PropertySourceFactory {
+
+    public PropertySource<?> createPropertySource(String name, EncodedResource encodedResource)
+            throws IOException {
+        try {
+            YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+            factory.setResources(encodedResource.getResource());
+
+            Properties properties = factory.getObject();
+
+            return new PropertiesPropertySource(encodedResource.getResource().getFilename(), properties);
+        } catch (IllegalStateException e) {
+            // for ignoreResourceNotFound
+            Throwable cause = e.getCause();
+            if (cause instanceof FileNotFoundException)
+                throw (FileNotFoundException) e.getCause();
+            throw e;
+        }
+    }
+}
