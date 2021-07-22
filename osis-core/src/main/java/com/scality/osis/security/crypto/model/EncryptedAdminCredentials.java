9565ec3363151da2eb8f62bd2f465f4b814333cc
saitharunthodupunuru <saitharun.thodupunuru@scality.com>
2021-07-22 00:45:53 -0700

    feature:S3C-4673 Implement Decrypt method for admin creds
    Description:
    * Reuse HKDF code from SQUARE/KEYWIZ library
    * Logic to derive the key from master keyfile using hkdf
    * Decrypt the encrypted admin credentials using the derived key

---
+++ osis-core/src/main/java/com/scality/osis/security/crypto/model/EncryptedAdminCredentials.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2021 Scality, Inc.
+ * SPDX-License-Identifier: Apache License 2.0
+ */
+
+package com.scality.osis.security.crypto.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EncryptedAdminCredentials {
+    String salt;
+    String tag;
+    String value;
+}
