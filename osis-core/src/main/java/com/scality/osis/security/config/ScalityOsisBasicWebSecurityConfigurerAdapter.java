4738faa499b7a596213cb6ca1e19672019ec8790
Anurag Mittal <anurag.mittal@scality.com>
2022-07-03 21:31:07 +0200

    feature: OSIS-27-add-password-ecoder

--- osis-core/src/main/java/com/scality/osis/security/config/ScalityOsisBasicWebSecurityConfigurerAdapter.java
+++ osis-core/src/main/java/com/scality/osis/security/config/ScalityOsisBasicWebSecurityConfigurerAdapter.java
@@ -9,16 +9,21 @@ package com.scality.osis.security.config;
 import com.scality.osis.security.basic.ScalityBasicAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static com.scality.osis.utils.ScalityConstants.HEALTH_CHECK_ENDPOINT;
 import static com.scality.osis.security.jwt.AuthConstants.API_INFO;
 import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+import com.scality.osis.security.platform.PlatformUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +36,15 @@ public class ScalityOsisBasicWebSecurityConfigurerAdapter extends WebSecurityCon
     @Autowired
     private ScalityBasicAuthentication authentication;
 
+    @Autowired
+    private PlatformUserDetailsService service;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(service).passwordEncoder(getPasswordEncoder());
+        auth.eraseCredentials(true);
+    }
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
@@ -39,5 +53,9 @@ public class ScalityOsisBasicWebSecurityConfigurerAdapter extends WebSecurityCon
         http.authorizeRequests().anyRequest().authenticated();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.httpBasic().authenticationEntryPoint(authentication);
+    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
