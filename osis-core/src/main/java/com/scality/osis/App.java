ff426f50c79def84b03000a7bdd2d855f471d50e
Olivier MATHE <omathe@free.fr>
2022-12-09 10:12:50 +0100

    OSIS-130 set osis version only once

---
+++ osis-core/src/main/java/com/scality/osis/App.java
@@ -0,0 +1,11 @@
+package com.scality.osis;
+
+/*
+This interface is updated by the Gradle task 'app' or each time the task compileJava is called.
+Don't change the structure as it is linked to the app task.
+ */
+public interface App {
+
+    String VERSION = "2.1.0";
+    Long DATE = 1670576298680L;
+}
--- osis-core/src/main/java/com/scality/osis/configuration/DocumentationConfig.java
+++ osis-core/src/main/java/com/scality/osis/configuration/DocumentationConfig.java
@@ -6,27 +6,33 @@
 
 package com.scality.osis.configuration;
 
+import com.scality.osis.App;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Configuration
 public class DocumentationConfig {
 
     private static final String DOC_TITLE = "Object Storage Interoperability Services API for Scality platform";
     private static final String DOC_DESCRIPTION = "This is VMware Cloud Director Object Storage Interoperability Services API for Scality platform.";
-    private static final String DOC_VERSION = "2.1.0";
     public static final String PROJECT_BASE = "com.scality.osis";
 
     @Bean
     public OpenAPI openAPI() {
 
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
+        String buildDate = formatter.format(new Date(App.DATE));
+
         return new OpenAPI()
                 .info(new Info()
                         .title(DOC_TITLE)
-                        .version(DOC_VERSION)
-                        .description(DOC_DESCRIPTION)
+                        .version(App.VERSION)
+                        .description(DOC_DESCRIPTION + "<br/>Build date : " + buildDate)
                 );
     }
 
